85e823c015e03ec231fc3cf5e064a4fcfa02babc
Jason Miller <jason.miller@inl.gov>
2020-01-15 15:02:31 -0700

    Attempt to limit output to errors
    It would seem, if we are building libMesh (let alone everything), the
    output goes over the limit of Civet's allowed size. Try to only print
    the error.

--- build_packages.sh
+++ build_packages.sh
@@ -74,10 +74,13 @@ exitIfReturnCode $?
 
 for formula in ${FORMULAS}; do
     printf "Building $(basename $formula)...\n"
-    print_and_run conda build -c https://mooseframework.org/conda/moose $formula
-    exitIfReturnCode $?
+    printf "conda build -c https://mooseframework.org/conda/moose $formula"
+    conda build -c https://mooseframework.org/conda/moose $formula > possible_errors 2>&1
+    if [ $? -ne 0 ]; then
+        cat possible_errors
+        exit 1
+    fi
 done
-
 
 if [ `uname` = 'Linux' ]; then
     ARCH='linux-64'
